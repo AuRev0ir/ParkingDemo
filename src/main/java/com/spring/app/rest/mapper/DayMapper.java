0f27483e7b1d19671078b5c6ce791ace42de7197
AyRevoir <101286467+AuRev0ir@users.noreply.github.com>
2022-11-02 10:16:38 +0300

    +

--- src/main/java/com/spring/app/rest/mapper/DayMapper.java
+++ src/main/java/com/spring/app/rest/mapper/DayMapper.java
@@ -1,14 +1,9 @@
 package com.spring.app.rest.mapper;
 
-import com.spring.app.dao.model.City;
 import com.spring.app.dao.model.Day;
-import com.spring.app.dao.model.Parking;
-import com.spring.app.rest.dto.CityDto;
 import com.spring.app.rest.dto.DayDto;
-import com.spring.app.rest.dto.ParkingDto;
 import org.mapstruct.Mapper;
 
-import java.util.List;
 import java.util.Set;
 
 @Mapper
